6528929037d76a854dffa382b073a039a747a883
NatiIP <153516449+NatiIP@users.noreply.github.com>
2024-05-05 22:05:32 +0300

    w

--- Server/FinalProject/FinalProject/obj/Debug/net6.0/FinalProject.AssemblyInfo.cs
+++ Server/FinalProject/FinalProject/obj/Debug/net6.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceff909ee3552a0beab5a3725081b1c79e86655a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccda90340722f2d34768bcf52939a4fd507d73e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
