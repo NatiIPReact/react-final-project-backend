ccda90340722f2d34768bcf52939a4fd507d73e3
NatiIP <153516449+NatiIP@users.noreply.github.com>
2024-05-05 14:08:31 +0300

    d

--- Server/FinalProject/FinalProject/obj/Debug/net6.0/FinalProject.AssemblyInfo.cs
+++ Server/FinalProject/FinalProject/obj/Debug/net6.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414c2d17bb50376ef26cba3a9e655876dfed30ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceff909ee3552a0beab5a3725081b1c79e86655a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Server/FinalProject/FinalProject/obj/Release/net6.0/FinalProject.AssemblyInfo.cs
+++ Server/FinalProject/FinalProject/obj/Release/net6.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5289ccd09845713fdcede079f9b20e17f740442")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceff909ee3552a0beab5a3725081b1c79e86655a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
