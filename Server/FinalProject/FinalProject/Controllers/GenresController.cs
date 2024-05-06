87d24d4c6752938158b0101234b7d1b115acb910
NatiIP <153516449+NatiIP@users.noreply.github.com>
2024-05-07 02:48:48 +0300

    thatsittt

--- Server/FinalProject/FinalProject/Controllers/GenresController.cs
+++ Server/FinalProject/FinalProject/Controllers/GenresController.cs
@@ -1,4 +1,5 @@
 ﻿using FinalProject.Models;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
--- Server/FinalProject/FinalProject/obj/Release/net6.0/FinalProject.AssemblyInfo.cs
+++ Server/FinalProject/FinalProject/obj/Release/net6.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceff909ee3552a0beab5a3725081b1c79e86655a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6528929037d76a854dffa382b073a039a747a883")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
